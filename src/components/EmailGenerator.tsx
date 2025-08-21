38c07d8f703f5ba64d319ef31d24de8a8e192477
tevahdesign <tevahdesign@gmail.com>
2025-08-21 20:08:55 +0530

    Update EmailGenerator.tsx

--- src/components/EmailGenerator.tsx
+++ src/components/EmailGenerator.tsx
@@ -42,6 +42,22 @@ export const EmailGenerator = () => {
           </button>
         </div>
       </div>
+      <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2885050972904135"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-2885050972904135"
+     data-ad-slot="9124471463"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+        `,
+      }}
+    />
     </div>
   );
 };
