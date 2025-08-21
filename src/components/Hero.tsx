d39de7e1ecdd2df229dd2324f7904fc0c42432e2
tevahdesign <tevahdesign@gmail.com>
2025-08-21 20:03:15 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,3 @@
-
-import { ArrowRight, Shield, Clock, Mail } from 'lucide-react';
-import { Link } from 'react-router-dom';
-
 export const Hero = () => {
-  return (
-    
-
-    
-  );
+  return null;
 };
