a6f957671fc0ae360e63e2484ea76b502c2bf708
tevahdesign <tevahdesign@gmail.com>
2025-08-21 19:43:04 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,29 +6,20 @@ export const Hero = () => {
   return (
     <div className="bg-app-darker py-12 sm:py-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2885050972904135"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-format="autorelaxed"
-     data-ad-client="ca-pub-2885050972904135"
-     data-ad-slot="9124471463"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+        <div className="text-center">
+        
           
           <div className="mt-8 flex justify-center">
             <div className="inline-flex rounded-md shadow">
               
             </div>
           </div>
-        </div>
+        
       
         
         </div>
-      
+      </div>
     
     
   );
 };
-
