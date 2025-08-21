26c1bd805d3f8287211679debe72c88e88be0423
tevahdesign <tevahdesign@gmail.com>
2025-08-21 20:05:33 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,20 @@
 export const Hero = () => {
-  return null;
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2885050972904135"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-2885050972904135"
+     data-ad-slot="9124471463"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+        `,
+      }}
+    />
+  );
 };
