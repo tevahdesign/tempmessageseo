32f368ff4af956c5ced096a7fece2a98c87af4a2
tevahdesign <tevahdesign@gmail.com>
2025-08-21 19:47:51 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,21 +4,7 @@ import { Link } from 'react-router-dom';
 
 export const Hero = () => {
   return (
-    <div className="bg-app-darker py-12 sm:py-16">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center">
-        
-          
-          <div className="mt-8 flex justify-center">
-            <div className="inline-flex rounded-md shadow">
-              
-            </div>
-          </div>
-        </div>
-      
-        
-        </div>
-      </div>
+    
     
     
   );
