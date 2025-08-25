78df140e66a1b26073cd2a2752012b74941e1565
tevahdesign <tevahdesign@gmail.com>
2025-08-25 23:26:26 +0530

    Update EmailInfo.tsx

--- src/components/EmailInfo.tsx
+++ src/components/EmailInfo.tsx
@@ -1,28 +1,22 @@
-
-import { Timer, CircleDot, ShieldCheck } from 'lucide-react';
-
 export const EmailInfo = () => {
   return (
-    <div className="space-y-2 px-6 py-4">
-      <h1 className="text-3xl font-bold text-white">Free Temporary Email.</h1>
-      <p className="text-gray-300 text-lg">
-        Receive emails anonymously with our free, private, and secure temporary email address generator
-      </p>
-      
-      <div className="flex flex-wrap gap-6 items-center mt-6">
-        <div className="flex items-center gap-2">
-          <Timer className="text-gray-400" size={20} />
-          <span className="text-gray-300">Valid for 1 hour</span>
-        </div>
-        <div className="flex items-center gap-2">
-          <CircleDot className="text-gray-400" size={20} />
-          <span className="text-gray-300">Free</span>
-        </div>
-        <div className="flex items-center gap-2">
-          <ShieldCheck className="text-gray-400" size={20} />
-          <span className="text-gray-300">Secure</span>
-        </div>
-      </div>
-    </div>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2885050972904135"
+     crossorigin="anonymous"></script>
+<!-- 3d -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2885050972904135"
+     data-ad-slot="1757117337"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+        `,
+      }}
+    />
   );
 };
