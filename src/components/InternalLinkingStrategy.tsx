258879347dbbe162aa68f6687c5bda8851c515c5
tevahdesign <tevahdesign@gmail.com>
2025-08-25 23:23:09 +0530

    Update InternalLinkingStrategy.tsx

--- src/components/InternalLinkingStrategy.tsx
+++ src/components/InternalLinkingStrategy.tsx
@@ -3,7 +3,17 @@ export const InternalLinkingStrategy = () => {
     <div
       dangerouslySetInnerHTML={{
         __html: `
-          <script></div>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2885050972904135"
+     crossorigin="anonymous"></script>
+<!-- 3d -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2885050972904135"
+     data-ad-slot="1757117337"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
         `,
       }}
