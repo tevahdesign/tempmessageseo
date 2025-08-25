0ceca9b45d40cb8676aabedb3d6113652530b8c8
tevahdesign <tevahdesign@gmail.com>
2025-08-25 09:55:16 +0530

    Update InboxDisplay.tsx

--- src/components/InboxDisplay.tsx
+++ src/components/InboxDisplay.tsx
@@ -1,4 +1,23 @@
-
+export const Hero = () => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2885050972904135"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-2885050972904135"
+     data-ad-slot="9124471463"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+        `,
+      }}
+    />
+  );
+};
 import { Mail, RefreshCw, Trash2, Eye } from 'lucide-react';
 import EmptyInboxImage from '../components/EmptyInboxImage';
 import { useEmail } from '../contexts/EmailContext';
