73f4cdf5782951ae9d891d8acb9be7b9fed11bb5
tevahdesign <tevahdesign@gmail.com>
2025-09-05 10:58:23 +0530

    Update InternalLinkingStrategy.tsx

--- src/components/InternalLinkingStrategy.tsx
+++ src/components/InternalLinkingStrategy.tsx
@@ -1,17 +1,12 @@
-import React from "react";
-
-const RawHtml: React.FC = () => {
-  const html = `
-    <div class="container">
-      <h1 style="color:red;">Hello World</h1>
-      <p>This is a paragraph.</p>
-      <script>
-        console.log('This runs like a script tag');
-      </script>
-    </div>
-  `;
-
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
-};
-
-export default RawHtml;
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2885050972904135"
+     crossorigin="anonymous"></script>
+<!-- Disp -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2885050972904135"
+     data-ad-slot="8766008078"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
