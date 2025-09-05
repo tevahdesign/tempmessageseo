d885079ea2410373632451a8a17310c384b19aff
tevahdesign <tevahdesign@gmail.com>
2025-09-05 10:41:43 +0530

    Update gStrategy.tsx

--- src/components/InternalLinkingStrategy.tsx
+++ src/components/InternalLinkingStrategy.tsx
@@ -1,36 +1,12 @@
-import React, { useEffect } from "react";
-
-const Adsense: React.FC = () => {
-  useEffect(() => {
-    // Dynamically add AdSense script
-    const script = document.createElement("script");
-    script.src =
-      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-286858050972904135";
-    script.async = true;
-    script.crossOrigin = "anonymous";
-    document.body.appendChild(script);
-
-    script.onload = () => {
-      try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-      } catch (e) {
-        console.error(e);
-      }
-    };
-  }, []);
-
-  return (
-    <div>
-      <ins
-        className="adsbygoogle" // must use className
-        style={{ display: "block" }} // style must be JS object
-        data-ad-client="ca-pub-2885050972904135"
-        data-ad-slot="8766008078"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-    </div>
-  );
-};
-
-export default Adsense;
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2885050972904135"
+     crossorigin="anonymous"></script>
+<!-- Disp -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2885050972904135"
+     data-ad-slot="8766008078"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
